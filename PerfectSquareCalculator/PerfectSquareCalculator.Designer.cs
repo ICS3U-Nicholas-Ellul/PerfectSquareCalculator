eccc8f036be86ee3de1d2e7c7396f92a2a9f9c8a
pirates19 <nicholas.ellul@ocsbstudent.ca>
2015-10-13 13:17:08 -0400

    Finished

---
+++ PerfectSquareCalculator/PerfectSquareCalculator.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace PerfectSquareCalculator
+{
+    partial class frmPerfectSquareCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstOutput = new System.Windows.Forms.ListBox();
+            this.nudInput = new System.Windows.Forms.NumericUpDown();
+            this.lblInstructions = new System.Windows.Forms.Label();
+            this.btnGo = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nudInput)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lstOutput
+            // 
+            this.lstOutput.FormattingEnabled = true;
+            this.lstOutput.Location = new System.Drawing.Point(43, 87);
+            this.lstOutput.Name = "lstOutput";
+            this.lstOutput.Size = new System.Drawing.Size(198, 134);
+            this.lstOutput.TabIndex = 0;
+            // 
+            // nudInput
+            // 
+            this.nudInput.Location = new System.Drawing.Point(82, 61);
+            this.nudInput.Name = "nudInput";
+            this.nudInput.Size = new System.Drawing.Size(120, 20);
+            this.nudInput.TabIndex = 1;
+            // 
+            // lblInstructions
+            // 
+            this.lblInstructions.AutoSize = true;
+            this.lblInstructions.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInstructions.Location = new System.Drawing.Point(25, 9);
+            this.lblInstructions.Name = "lblInstructions";
+            this.lblInstructions.Size = new System.Drawing.Size(247, 40);
+            this.lblInstructions.TabIndex = 2;
+            this.lblInstructions.Text = "Enter a number to see all the \r\npefect squares to that value";
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(104, 227);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 3;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // frmPerfectSquareCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.lblInstructions);
+            this.Controls.Add(this.nudInput);
+            this.Controls.Add(this.lstOutput);
+            this.Name = "frmPerfectSquareCalculator";
+            this.Text = "Perfect Squares";
+            ((System.ComponentModel.ISupportInitialize)(this.nudInput)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstOutput;
+        private System.Windows.Forms.NumericUpDown nudInput;
+        private System.Windows.Forms.Label lblInstructions;
+        private System.Windows.Forms.Button btnGo;
+    }
+}
+
---
+++ PerfectSquareCalculator/PerfectSquareCalculator.cs
@@ -0,0 +1,56 @@
+﻿/*
+ * Created by: Nicholas Ellul
+ * Created on: 13-Oct-2015
+ * Created for: ICS3U
+ * Daily Assignment – Unit 3-09
+ * This program lists perfect squares up to a certain number
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PerfectSquareCalculator
+{
+    public partial class frmPerfectSquareCalculator : Form
+    {
+        public frmPerfectSquareCalculator()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            //input
+            int inputValue = Convert.ToInt32(this.nudInput.Value);
+            int counter = 0;
+            double squareRootAsDouble;
+            int squareRootAsInt;
+
+            //process
+            this.lstOutput.Items.Clear();
+            while (counter < inputValue)
+            {
+                squareRootAsDouble = Math.Sqrt(counter);
+                squareRootAsInt = Convert.ToInt32(squareRootAsDouble);
+
+                if (squareRootAsDouble == squareRootAsInt)
+                {
+                    //output
+                    this.lstOutput.Items.Add(counter);
+
+
+                }
+
+                counter++;
+            }
+           
+        }
+    }
+}
